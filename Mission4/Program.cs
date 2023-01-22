21ee7e9cf4ec1243ec67213f3a2224a9a194c114
Tatyana <tatyana.kurii@mail.ru>
2023-01-22 22:24:00 +0600

    Mission4

---
+++ Mission4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу вычисления функции:
+//  x = f(a)
+
+Console.Write("Введите число, которое будете возводить в куб > ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int x = a * a * a;
+
+Console.WriteLine ($"Куб числа {a} равен {x}");
+
+
+
+
