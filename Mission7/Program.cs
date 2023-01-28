8d9509955755446b4f6475bfd1c6a82fefd363ea
Tatyana <tatyana.kurii@mail.ru>
2023-01-28 15:39:41 +0600

    Mission7

---
+++ Mission7/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает значение, а  на выходе показывает обратное значение.
+// 1 -> 1 
+// 2 -> 0.5
+// 0.25 -> 4
+
+double Prompt(string message)
+{
+Console.WriteLine(message);
+return Convert.ToDouble(Console.ReadLine());
+}
+
+double number = Prompt("Введите число");
+double result = 1/number;
+System.Console.WriteLine($"Обратное число равно {result}");
