af05ca54a0a1c61d5bead5f104d84f420de47bb2
Tatyana <tatyana.kurii@mail.ru>
2023-01-28 16:00:53 +0600

    Mission8

---
+++ Mission8/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу вычисления модуля числа.
+	// 2 -> 2
+	// -3 -> 3
+	// -7 -> 7
+
+double Prompt(string message)
+{
+Console.WriteLine(message);
+return Convert.ToDouble(Console.ReadLine());
+}
+
+double number = Prompt("Введите число");
+double result = 1 / number;
+
+double abs;
+if (number > 0)
+{
+abs = number;
+}
+else
+{
+abs = -number;
+}
+
+System.Console.WriteLine($"Обратное число равно {result}");
+System.Console.WriteLine($"Модуль числа равно {abs}");
